421f68cb8a43a2cd97108cd59b3181e5f019ef12
Aakarsh Srivastava <132938129+Aakarshk25@users.noreply.github.com>
2023-05-28 21:34:47 +0530

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,58 @@
+// how add eventlistnerwork.
+//he addEventListener() method of the EventTarget interface sets up a 
+//function that will be called whenever the specified event is delivered to the target.
+//Common targets are Element, or its children, Document, and Window,
+// but the target may be any object that supports events (such as XMLHttpRequest).
+
+
+
+
+
+// document.querySelector("button").addEventListener("click", function () {
+//     var buttons = document.querySelectorAll("button");
+//     for (var i = 0; i < buttons.length; i++) {
+//         alert("hi");
+//     }
+
+// }
+
+var numberOfbuttons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfbuttons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+            case "w":
+                var tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
+                break;
+            case "a":
+                var tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+                break;
+            case "s":
+                var tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();
+                break;
+            case "d":
+                var tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
+                break;
+            case "j":
+                var snare = new Audio('sounds/snare.mp3');
+                snare.play();
+                break;
+            case "k":
+                var crash = new Audio('sounds/crash.mp3');
+                crash.play();
+                break;
+            case "l":
+                var kick = new Audio('sounds/kick-bass.mp3');
+                kick.play();
+                break;
+
+            default:
+                break;
+        }
+    });
+}
